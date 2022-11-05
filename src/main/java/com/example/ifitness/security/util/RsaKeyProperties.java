8c41a67f976cc58f11a91ce1848b3e3c504e9228
Jason <officialjasonzeng@gmail.com>
2022-11-05 16:09:09 -0400

    deploying

--- src/main/java/com/example/ifitness/security/util/RsaKeyProperties.java
+++ src/main/java/com/example/ifitness/security/util/RsaKeyProperties.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 
-@ConfigurationProperties(prefix = "rsa")
+//@ConfigurationProperties(prefix = "rsa")
 public record RsaKeyProperties(RSAPublicKey publicKey, RSAPrivateKey privateKey) {
 
 }
